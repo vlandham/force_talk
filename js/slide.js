2ba40984637d971045314898a7acd2463163b655
Jim V <vlandham@gmail.com>
2013-02-11 11:25:48 -0600

    adding more form. adding goofy videos. hoping not to kill my machine with this talk

--- js/slide.js
+++ js/slide.js
@@ -10,7 +10,7 @@ window.onload = setup;
 
 function setup() {
   src = parentWindow.document.getElementById(window.name).src;
-  setupCode();
+  // setupCode();
   
   window.onmessage = receiveMessage;
 }
@@ -56,5 +56,20 @@ function receiveMessage(e) {
     } 
   }
   return true;
+}
+
+/**
+ * Returns a random number between min and max
+ */
+function getRandomArbitary (min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min and max
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
